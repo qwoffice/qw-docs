e0869c19351c67375ff0875e461c4265e95cb995
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2023-01-05 13:38:59 -0500

    Create legacy-loader.php

---
+++ laravel/config/legacy-loader.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'route_prefix' => env('LEGACY_ROUTE_PREFIX', 'legacy'),
+    /**
+     * Path to legacy php scripts
+     * relative to base_path()
+     */
+    'file_path' => env('LEGACY_FILE_PATH', 'resources/legacy'),
+    /**
+     * If your legacy app has its own
+     * authentication you will need
+     * to publish config and add
+     * your own middleware.
+     */
+    'middleware' => [
+        'web',
+        'auth',
+        'team.auth'
+    ],
+];
